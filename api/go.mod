70d1f2fe217b3ab0a804411b2feb9e1740331365
Ivan Salinas <contactoivansalinasss@gmail.com>
2024-03-14 23:09:01 -0300

    Se agrega api

---
+++ api/go.mod
@@ -0,0 +1,9 @@
+module github.com/hugohb/gym-fusion-app/api
+
+go 1.22.1
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/hugohb/gym-fusion-app/api/handlers"
+	"github.com/hugohb/gym-fusion-app/api/server"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	s, err := server.NewServer(context.Background(), &server.Config{
+		JWTSecret:   JWT_SECRET,
+		Port:        PORT,
+		DatabaseUrl: DATABASE_URL,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.Start(BindRoutes)
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+}
